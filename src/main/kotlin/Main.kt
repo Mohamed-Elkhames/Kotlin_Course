c693b50884f61992670ed53ac2a0a2b221dfe095
Mohamed <mohamedelkhames1552gmail.com>
2023-02-18 12:33:37 +0200

    this is update

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,30 +1,30 @@
 fun main(){
-    val age = readLine()!!.toInt()
-    if (age < 18){
-        println("you are young")
-    }else if (age > 18){
-        println("you are adult")
-    }else {
-        println("you are die")
-    }
-    val x = readLine()!!.toInt()
-    when(x){
-        0 -> println("sign in")
-        1 -> println("sign out")
-        2 -> println("Exit")
-        else -> println("Invalid")
-    }
-    val arr = IntArray(5)
-    arr[0]= 1
-    arr[1]= 2
-    arr[2]= 3
-    arr[3]= 4
-    arr[4]= 5
+//    val age = readLine()!!.toInt()
+//    if (age < 18){
+//        println("you are young")
+//    }else if (age > 18){
+//        println("you are adult")
+//    }else {
+//        println("you are die")
+//    }
+//    val x = readLine()!!.toInt()
+//    when(x){
+//        0 -> println("sign in")
+//        1 -> println("sign out")
+//        2 -> println("Exit")
+//        else -> println("Invalid")
+//    }
+    val arr = IntArray(5){it + 1} // it mean sum the number in the index to each other
+//    arr[0]= 1
+//    arr[1]= 2
+//    arr[2]= 3
+//    arr[3]= 4
+//    arr[4]= 5
     var result = 0
-    for (i in arr){
-        result+=i
+    for (i in arr.indices){
+        result+=arr[i]
     }
 
-    println(result)
+    println("result = $result")
 
 }
