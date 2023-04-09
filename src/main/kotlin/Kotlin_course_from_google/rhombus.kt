41c1db187524b71ebd6eacbf6c8b3946cb818fe3
Mohamed-Elkhames <110728900+Mohamed-Elkhames@users.noreply.github.com>
2023-04-09 15:15:42 +0200

    triangle and rhombus

--- src/main/kotlin/Kotlin_course_from_google/rhombus.kt
+++ src/main/kotlin/Kotlin_course_from_google/rhombus.kt
@@ -19,3 +19,19 @@ fun main() {
         println()
     }
 }
+
+// this is triangle with stars 
+fun main() {
+    for(i in 1 ..5){
+        val numberOfSpaces = 5 - i
+        for (j in 1..numberOfSpaces){
+            print(" ")
+        }
+        // i * 2 - 1 will be 1 * 2 - 1 = 1 then 2 * 2 - 1 = 3 then 3 * 2 - 1 = 5  ...etc 
+        val numberOfStars = i * 2 - 1 
+        for(j in 1..numberOfStars){
+            print("*")
+        }
+        println()
+    }
+}
