f1640b8e15ebf57722a824c03802f2de9173727f
Mohamed-Elkhames <110728900+Mohamed-Elkhames@users.noreply.github.com>
2023-04-09 23:22:15 +0200

    nasted loop exercises

--- src/main/kotlin/Kotlin_course_from_google/rhombus.kt
+++ src/main/kotlin/Kotlin_course_from_google/rhombus.kt
@@ -35,3 +35,44 @@ fun main() {
         println()
     }
 }
+// another triangle 
+fun main() {
+    for (i in 1..5){
+        val numberOfSpaces = i - 1
+        for (j in 1..numberOfSpaces){
+            print(" ")
+        }
+        val numberOfStars = 11 - i * 2 // 11 - 1 * 2 = 9
+        for (j in 1..numberOfStars){
+            print("*")
+        }
+        println()
+
+    }
+}
+
+// another tringle
+fun main() {
+for (i in 1 until 5) {
+   val numberOfSpaces = 5 - i
+for (j in 1..numberOfSpaces) {
+   print(" ")
+}
+val numberOfStars = i
+for (j in 1..numberOfStars) {
+     print("*")
+}
+     println()
+}
+for (i in 1..5) {
+val numberOfSpaces = i - 1
+for (j in 1..numberOfSpaces) {
+      print(" ")
+}
+val numberOfStars = 6 - i
+for (j in 1..numberOfStars) {
+     print("*")
+}
+     println()
+}
+}
